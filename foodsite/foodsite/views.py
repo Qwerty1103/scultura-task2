f36990272f0c8885a622ddc10be0f53872bf6c99
Your Name <you@example.com>
2022-09-23 23:47:59 +0530

    Added AJAX to handle + operation

--- foodsite/foodsite/views.py
+++ foodsite/foodsite/views.py
@@ -1,13 +1,23 @@
+from asyncio.windows_events import NULL
+from contextlib import nullcontext
+import string
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import redirect,render
 
-def homePage(request):
-    return render(request,"index.html")
+def homePage(request, ids = 0, qtys = 0):
+    return render(request,"index.html", {'ids': ids, 'qtys': qtys})
 
+@csrf_exempt
 def setCart(request):
-    request.session['sname'] = ['1','2','3']
-    return HttpResponse('Session')
+    if request.method == "POST":
+        request.session['ids'] = request.POST.get('iID')
+        request.session['qtys'] = request.POST.get('qty')
+    return getCart(request)
 
 def getCart(request):
-    cartitems = request.session['sname']
-    return HttpResponse(cartitems)
+    ids = request.session['ids']
+    qtys = request.session['qtys']
+    ids = 1
+    qtys = 1
+    return redirect('/', ids, qtys)
