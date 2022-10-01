a906954f5b8dbd30cef72619a1037add2a953d52
Your Name <you@example.com>
2022-10-01 17:06:21 +0530

    Items now display in cart

--- foodsite/foodsite/views.py
+++ foodsite/foodsite/views.py
@@ -2,6 +2,7 @@ from asyncio.windows_events import NULL
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect, render
+from flask import jsonify
 
 @csrf_exempt
 def updateItem(request):
@@ -18,18 +19,21 @@ def updateItem(request):
         for item in cart:
             if item.get('id') == id:
                 flag = 1
-                item.update({'qty': qty})
+                item.update({"qty": qty})
                 request.session['cart'] = cart
         if flag == 0:
-            data = {'id': id, 'qty': qty}
+            data = {"id": id, "qty": qty}
             cart.append(data)
             request.session['cart'] = cart
         print(cart)
-    return JsonResponse({'cart': cart})
+    return JsonResponse({"cart": cart})
 
+@csrf_exempt
 def homePage(request):
     cart = request.session['cart']
-    return render(request,"index.html", {'cart': cart})
+    if(request.method == "POST"):
+        return JsonResponse({"cart": cart})
+    return render(request,"index.html", {"cart": cart})
 
 def reset(request):
     request.session['cart'] = []
